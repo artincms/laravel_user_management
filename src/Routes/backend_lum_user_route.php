c6b2bbc4e44a488a101e9ad311f5825da98c84de
adel <adel.raheli@gmail.com>
2018-10-05 02:06:10 +0330

    1

--- src/Routes/backend_lum_user_route.php
+++ src/Routes/backend_lum_user_route.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['prefix' => config('laravel_user_management.backend_lum_route_prefix'), 'namespace' => config('laravel_user_management.external_user_controller_name_space'), 'middleware' => config('laravel_user_management.backend_lum_middlewares')], function () {
+Route::group(['prefix' => config('laravel_user_management.backend_lum_route_prefix'), 'namespace' => config('laravel_user_management.user_controller_name_space'), 'middleware' => config('laravel_user_management.backend_lum_middlewares')], function () {
     Route::group(['prefix' => 'Users'], function () {
         Route::get('index', ['as' => 'LUM.Users.index', 'uses' => config('laravel_user_management.user_controller_name') . '@index']);
         Route::post('getUsers', ['as' => 'LUM.Users.getUsers', 'uses' => config('laravel_user_management.user_controller_name') . '@getUsers']);
